0c10a1ba7c697ada6bc2c73481a384d97fc49aac
Ridouane GHERMI <ghermi@titan.lix.polytechnique.fr>
2025-07-07 17:00:50 +0200

    Added the baseline submission file

--- scripts/run_inference.py
+++ scripts/run_inference.py
@@ -1,11 +1,15 @@
 import os
 import argparse
-import numpy as np
 import pandas as pd
-import cv2
-from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor
-from qwen_vl_utils import process_vision_info
 from tqdm import tqdm
+from datasets import load_dataset
+from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor
+
+# 'pip install qwen-vl-utils'
+from qwen_vl_utils import process_vision_info
+
+
+#WIDTHS = {144: 256, 240: 426, 360: 640, 480: 854, 720: 1280, 1080: 1920}
 
 
 TEMPLATE = (
@@ -18,40 +22,23 @@ TEMPLATE = (
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_path", type=str, required=True)
-    parser.add_argument("--subtitles_path", type=str, required=True)
-    parser.add_argument("--output_path", type=str, required=True)
-    parser.add_argument("--video_dir", type=str, required=True)
-    parser.add_argument("--model_dir", type=str, required=True)
-    parser.add_argument("--num_frames", type=int, default=16)
-    parser.add_argument("--input_modality", type=str, default="vl", choices=["v", "l", "vl"])
+    parser.add_argument("--split", type=str, default="test_expert")
+    parser.add_argument("--subtitles_path", type=str, default="../data/test_subtitles.csv")
+    parser.add_argument("--output_path", type=str, default="submission.csv")
+    parser.add_argument("--video_dir", type=str, default="../data/videos/")
+    parser.add_argument("--model_dir", type=str, default="")
+    parser.add_argument("--num_frames", type=int, default=8)
+    #parser.add_argument("--resolution", type=int, default=360, choices=[144, 240, 360, 480, 720, 1080], help="Resolution for video download (e.g., 360, 720, 1080)")    parser.add_argument("--input_modality", type=str, default="vl", choices=["v", "l", "vl"])
     parser.add_argument("--model_id", type=str, default="Qwen/Qwen2.5-VL-3B-Instruct")
     return parser.parse_args()
 
 
-def load_video(video_path: str, num_frames: int = 16):
-    """Load video frames from a given path."""
-    cap = cv2.VideoCapture(video_path)
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    indices = np.linspace(0, frame_count - 1, num_frames, dtype=int)
-
-    frames = []
-    for idx in indices:
-        cap.set(cv2.CAP_PROP_POS_FRAMES, idx)
-        success, frame = cap.read()
-        if success:
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            frames.append(frame)
-        else:
-            frames.append(np.zeros_like(frames[-1] if frames else np.zeros((64, 64, 3), dtype=np.uint8)))
-
-    cap.release()
-    return frames
-
-
 def main(args):
     # Prepare data
-    df = pd.read_csv(args.data_path)
+    #video_dir = os.path.join(args.video_dir, f"{args.resolution}p")
+    df = load_dataset("rghermi/sf20k", split=args.split).to_pandas()
+    df = df[['question_id', 'video_id', 'video_url', 'question']]
+    df = df.sample(n=32, random_state=42)
     df_subs = pd.read_csv(args.subtitles_path)
 
     # Prepare model
@@ -66,12 +53,20 @@ def main(args):
     predictions = []
     for i, sample in tqdm(df.iterrows(), total=len(df)):
         # Create the prompt
-        video_path = os.path.join(args.video_dir, f"{sample['video_id']}.mp4")
+        #video_path = os.path.join(video_dir, f"{sample['video_id']}.mp4")
+        #video_path = os.path.join(args.video_dir, f"{sample['video_id']}.mp4")
+        video_path = os.path.join(args.video_dir, f"{sample['video_id']}.mkv")
         subtitles = '\n'.join(df_subs[(df_subs.video_id == sample['video_id'])].text.tolist())
         prompt = TEMPLATE.format(subtitles=subtitles, question=sample['question'])
 
         messages = [{"role": "user", "content": [
-            {"type": "video", "video": video_path, "nframes": args.num_frames},
+            {
+                "type": "video",
+                "video": video_path,
+                "nframes": args.num_frames,
+                #"resized_height": args.resolution,
+                #"resized_width": WIDTHS[args.resolution],
+            },
             {"type": "text", "text": prompt}
         ]}]
 
@@ -107,6 +102,12 @@ def main(args):
 
         predictions.append(prediction)
 
+        # Save intermediate results
+        #if i % 50:
+        #    df['prediction'] = predictions
+        #    df.to_csv(args.output_path, index=False)
+
+    # Save results
     df['prediction'] = predictions
     df.to_csv(args.output_path, index=False)
 
