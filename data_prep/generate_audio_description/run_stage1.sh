4babc55e989c8cd0f6f2097ca505b82e4f9aac18
Ridouane GHERMI <ghermi@titan.lix.polytechnique.fr>
2025-10-08 17:40:07 +0200

    Added accelerate script for stage 1 AD generation

--- data_prep/generate_audio_description/run_stage1.sh
+++ data_prep/generate_audio_description/run_stage1.sh
@@ -32,3 +32,18 @@ python run_stage1.py \
     --num_frames 8 \
     --start_idx $START_IDX \
     --end_idx $END_IDX
+
+accelerate launch \
+    --config_file default_config.yaml \
+    --num_processes 2 \
+    run_stage1_accelerate.py \
+    --output_path ./results/stage1_qwen2vl-7b-$START_IDX-$END_IDX.json \
+    --data_path /lustre/fswork/projects/rech/kcn/ucm72yx/data/SF20K/questions/train.csv \
+    --shots_path /lustre/fswork/projects/rech/kcn/ucm72yx/data/SF20K/shots.parquet \
+    --video_dir /lustre/fswork/projects/rech/kcn/ucm72yx/data/SF20K/videos/ \
+    --weights_dir /lustre/fsmisc/dataset/HuggingFace_Models/ \
+    --model_id Qwen/Qwen2.5-VL-7B-Instruct \
+    --num_frames 8 \
+    --batch_size 8 \
+    --start_idx $START_IDX \
+    --end_idx $END_IDX
