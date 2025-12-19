5aca99efdce327544642bcfeeef37f16ca0ee753
Ridouane GHERMI <ghermi@titan.lix.polytechnique.fr>
2025-12-19 04:05:06 +0100

    minor changes

--- experiments/instruction_tuning/create_slurm.py
+++ experiments/instruction_tuning/create_slurm.py
@@ -19,6 +19,7 @@ module load ffmpeg/6.1.1
 module load pytorch-gpu/py3/2.6.0
 source /lustre/fsn1/projects/rech/kcn/ucm72yx/virtual_envs/sf20k/bin/activate
 cd /lustre/fswork/projects/rech/kcn/ucm72yx/code/sf20k/experiments/instruction_tuning/
+wandb offline
 
 accelerate launch \\
     --num_processes 2 \\
