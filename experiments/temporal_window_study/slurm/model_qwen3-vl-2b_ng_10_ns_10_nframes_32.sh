5172410c414b120bd445b844ccb9a44b440eb6a1
Ridouane GHERMI <ghermi@titan.lix.polytechnique.fr>
2026-01-09 18:42:08 +0100

    minor fixes

--- experiments/temporal_window_study/slurm/model_qwen3-vl-2b_ng_10_ns_10_nframes_32.sh
+++ experiments/temporal_window_study/slurm/model_qwen3-vl-2b_ng_10_ns_10_nframes_32.sh
@@ -16,6 +16,7 @@ module load ffmpeg/6.1.1
 module load pytorch-gpu/py3/2.6.0
 source /lustre/fsn1/projects/rech/kcn/ucm72yx/virtual_envs/sf20k/bin/activate
 cd /lustre/fswork/projects/rech/kcn/ucm72yx/code/sf20k/experiments/temporal_window_study/
+git checkout rig-dev
 
 python run_inference.py \
     --output_dir results \
