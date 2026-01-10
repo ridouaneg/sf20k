f3f0dc62b9e0c687747a90cb1e2ff8db332a2892
Ridouane GHERMI <ghermi@titan.lix.polytechnique.fr>
2026-01-10 16:02:47 +0100

    minor fix

--- experiments/instruction_tuning/slurm/qwen3vl2b_32f_8192.sh
+++ experiments/instruction_tuning/slurm/qwen3vl2b_32f_8192.sh
@@ -18,16 +18,18 @@ source /lustre/fsn1/projects/rech/kcn/ucm72yx/virtual_envs/sf20k/bin/activate
 cd /lustre/fsn1/projects/rech/kcn/ucm72yx/code/sf20k/experiments/instruction_tuning/
 wandb offline
 
+config_name=qwen3vl2b_32f_8192
+
 # Train
 accelerate launch \
     --num_processes 1 \
     --config_file default_config.yaml \
     train.py \
-    --config configs/qwen3vl2b_8f_8192.yaml
+    --config configs/${config_name}.yaml
 
 # Run inference
 python run_inference.py \
-    --output_dir ./results/qwen3vl2b_8f_8192/ \
+    --output_dir ./results/${config_name}/ \
     --data_path /lustre/fsn1/projects/rech/kcn/ucm72yx/code/sf20k/data/test_expert.csv \
     --subtitles_path /lustre/fsn1/projects/rech/kcn/ucm72yx/code/sf20k/data/test_subtitles.csv \
     --video_dir /lustre/fswork/projects/rech/kcn/ucm72yx/data/SF20K/videos/ \
@@ -39,8 +41,8 @@ python run_inference.py \
     --n_subsample -1
 
 python run_inference.py \
-    --output_dir ./results/qwen3vl2b_8f_8192/ \
-    --adapter_path ./results/qwen3vl2b_8f_8192/checkpoint-final/ \
+    --output_dir ./results/${config_name}/ \
+    --adapter_path ./results/${config_name}/checkpoint-final/ \
     --data_path /lustre/fsn1/projects/rech/kcn/ucm72yx/code/sf20k/data/test_expert.csv \
     --subtitles_path /lustre/fsn1/projects/rech/kcn/ucm72yx/code/sf20k/data/test_subtitles.csv \
     --video_dir /lustre/fswork/projects/rech/kcn/ucm72yx/data/SF20K/videos/ \
