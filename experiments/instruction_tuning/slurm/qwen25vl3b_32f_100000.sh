63d5a58dfe6a96113847c32397894ddfb78a8263
Ridouane GHERMI <ghermi@titan.lix.polytechnique.fr>
2026-01-14 23:46:00 +0100

    added qwen2.5-vl for instruction tuning

---
+++ experiments/instruction_tuning/slurm/qwen25vl3b_32f_100000.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#SBATCH --job-name=sf20k
+#SBATCH -A kcn@h100
+#SBATCH -C h100
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=10
+#SBATCH --hint=nomultithread
+#SBATCH --time=20:00:00
+#SBATCH --output=/lustre/fsn1/projects/rech/kcn/ucm72yx/slurm/sf20k/%j.out
+#SBATCH --error=/lustre/fsn1/projects/rech/kcn/ucm72yx/slurm/sf20k/%j.err
+
+module load arch/h100
+module load ffmpeg/6.1.1
+module load pytorch-gpu/py3/2.6.0
+source /lustre/fsn1/projects/rech/kcn/ucm72yx/virtual_envs/sf20k/bin/activate
+cd /lustre/fsn1/projects/rech/kcn/ucm72yx/code/sf20k/experiments/instruction_tuning/
+wandb offline
+
+config_name=qwen25vl3b_32f_100000
+
+# Train
+accelerate launch \
+    --num_processes 1 \
+    --config_file default_config.yaml \
+    train.py \
+    --config configs/${config_name}.yaml
+
+# Run inference
+python run_inference.py \
+    --output_dir ./results/${config_name}/ \
+    --data_path /lustre/fsn1/projects/rech/kcn/ucm72yx/code/sf20k/data/test_expert.csv \
+    --subtitles_path /lustre/fsn1/projects/rech/kcn/ucm72yx/code/sf20k/data/test_subtitles.csv \
+    --video_dir /lustre/fswork/projects/rech/kcn/ucm72yx/data/SF20K/videos/ \
+    --model_name qwen2.5-vl-3b \
+    --weights_dir /lustre/fsn1/projects/rech/kcn/ucm72yx/weights/ \
+    --modality vision_language \
+    --fps 1.0 \
+    --num_frames 32 \
+    --n_subsample -1
+
+python run_inference.py \
+    --output_dir ./results/${config_name}/ \
+    --adapter_path ./results/${config_name}/checkpoint-final/ \
+    --data_path /lustre/fsn1/projects/rech/kcn/ucm72yx/code/sf20k/data/test_expert.csv \
+    --subtitles_path /lustre/fsn1/projects/rech/kcn/ucm72yx/code/sf20k/data/test_subtitles.csv \
+    --video_dir /lustre/fswork/projects/rech/kcn/ucm72yx/data/SF20K/videos/ \
+    --model_name qwen2.5-vl-3b \
+    --weights_dir /lustre/fsn1/projects/rech/kcn/ucm72yx/weights/ \
+    --modality vision_language \
+    --fps 1.0 \
+    --num_frames 32 \
+    --n_subsample -1
---
+++ experiments/instruction_tuning/slurm/test_qwen25vl.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#SBATCH --job-name=sf20k
+#SBATCH -A kcn@h100
+#SBATCH -C h100
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=10
+#SBATCH --hint=nomultithread
+#SBATCH --time=20:00:00
+#SBATCH --output=/lustre/fsn1/projects/rech/kcn/ucm72yx/slurm/sf20k/%j.out
+#SBATCH --error=/lustre/fsn1/projects/rech/kcn/ucm72yx/slurm/sf20k/%j.err
+
+module load arch/h100
+module load ffmpeg/6.1.1
+module load pytorch-gpu/py3/2.6.0
+source /lustre/fsn1/projects/rech/kcn/ucm72yx/virtual_envs/sf20k/bin/activate
+cd /lustre/fsn1/projects/rech/kcn/ucm72yx/code/sf20k/experiments/instruction_tuning/
+wandb offline
+
+config=test_qwen25vl
+
+# Train
+CUDA_VISIBLE_DEVICES=1 accelerate launch \
+    --num_processes 1 \
+    --config_file default_config.yaml \
+    train.py \
+    --config configs/${config}.yaml
+
+# Run inference
+CUDA_VISIBLE_DEVICES=1 python run_inference.py \
+    --output_dir ./results/${config}/ \
+    --data_path /users/ghermi/code/sf20k/data/test_expert.csv \
+    --subtitles_path /users/ghermi/code/sf20k/data/test_subtitles.csv \
+    --video_dir /geovic/geovic/SF20K/videos/ \
+    --model_name qwen2.5-vl-3b \
+    --weights_dir /geovic/ghermi/weights/ \
+    --modality vision_language \
+    --num_frames 8 \
+    --fps 1.0 \
+    --n_subsample 8
+
+python run_inference.py \
+    --output_dir ./results/${config}/ \
+    --data_path /users/ghermi/code/sf20k/data/test_expert.csv \
+    --subtitles_path /users/ghermi/code/sf20k/data/test_subtitles.csv \
+    --video_dir /geovic/geovic/SF20K/videos/ \
+    --model_name qwen3-vl-2b \
+    --weights_dir /geovic/ghermi/weights/ \
+    --adapter_path ./results/${config}/checkpoint-final/ \
+    --modality vision_language \
+    --num_frames 8 \
+    --fps 1.0 \
+    --n_subsample 8
