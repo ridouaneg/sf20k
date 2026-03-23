1bca38e0417c280202a7a65c2f93d61eb66ea7a1
Ridouane GHERMI <ghermi@titan.lix.polytechnique.fr>
2026-03-23 13:42:30 +0100

    minor changes

--- sf20k/models/internvl.py
+++ sf20k/models/internvl.py
@@ -185,8 +185,8 @@ class InternVLModel:
             do_sample=True,
         )
 
-        pixel_values, num_patches_list = load_video(video_path, bound=None, input_size=448, max_num=1, num_segments=32)
-        #pixel_values, num_patches_list = load_video(video_path, bound=None, input_size=224, max_num=1, num_segments=self.num_frames)
+        #pixel_values, num_patches_list = load_video(video_path, bound=None, input_size=448, max_num=1, num_segments=32)
+        pixel_values, num_patches_list = load_video(video_path, bound=None, input_size=224, max_num=1, num_segments=self.num_frames)
         pixel_values = pixel_values.to(self.model.device, self.model.dtype)
 
         video_prefix = ''.join([f'Frame{i+1}: <image>\n' for i in range(len(num_patches_list))])
